1e1f4c3a4940a521a7017df579740ede906638c8
tori-takashi <tori.toutoshi@gmail.com>
2018-08-24 17:31:57 +0900

    scopeを定義する

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+    scope :costly, -> { where("price > ?", 3000)}
+    scope :written_about, -> (theme){ where("name like '#{theme}'")}
+    default_scope -> { order("price DESC")}
 end
