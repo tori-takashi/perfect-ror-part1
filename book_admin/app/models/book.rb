0a4fac8b83586f4b9717f9ffdaa49a029d3c5bef
tori-takashi <tori.toutoshi@gmail.com>
2018-08-25 17:00:50 +0900

    バリデーション

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -4,6 +4,16 @@ class Book < ActiveRecord::Base
     default_scope -> { order("price DESC")}
 
     belongs_to :publisher
+    
     has_many :book_authors
     has_many :authors, through: :book_authors
+
+    validates :name, presence: true
+    validates :name, length: { maximum: 15 }
+    validates :price, numericality: { greater_than_or_equal_to: 0 }
+    validate do |book|
+        if book.name.include?("ピーポくん")
+            book.errors[:name] << "そんな本があるはずありません"
+        end
+    end
 end
